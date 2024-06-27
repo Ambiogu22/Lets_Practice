4ab039994e4a2140bcf13efd327de2850c91c03c
Ambiogu22 <ambio22620@gmail.com>
2024-06-27 14:44:18 -0400

    Working on NavBar (Empty file)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './assets/style.css'
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <div>
-      <h2>test!!</h2>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element="Not Found!" />
+      </Routes>
     </div>
   );
 }
