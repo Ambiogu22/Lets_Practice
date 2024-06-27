4e45eeae2213919c713500fa51db0ee972d534c9
Ambiogu22 <ambio22620@gmail.com>
2024-06-27 04:23:03 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './style.css';
+import { Routes, Route} from 'react-router-dom'
+import Home from './Home';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
