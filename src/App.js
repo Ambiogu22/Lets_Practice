44d867e0b86e1bfb26fbbbf2e18c0e02360b7e88
Ambiogu22 <ambio22620@gmail.com>
2024-06-27 14:25:34 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './style.css';
-import { Routes, Route} from 'react-router-dom'
-import Home from './Home';
 
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <h2>test!!</h2>
     </div>
   );
 }
