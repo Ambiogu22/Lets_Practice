dace883de709a5c657e6e45675ac7471805a67a4
Ambiogu22 <ambio22620@gmail.com>
2024-07-01 13:37:56 -0400

    Working in Shop Page

--- src/pages/Shop/ShopItemCard.js
+++ src/pages/Shop/ShopItemCard.js
@@ -3,19 +3,19 @@ const ShopItemCard = (props) => {
 
     const item = props.item
     return(
-        <div className="flex flex-col bg-[#EEEEEE] px-4 py-6 flex justify-center items-center gap-8 rounded-lg">
-            <div className="flex max-w-32 max-h-40 flex-col justify-around items-center text-center">
+        <div className="flex flex-col bg-[#EEEEEE] px-4 py-6 flex justify-center items-center gap-12 rounded-lg">
+            <div className="flex max-w-32 max-h-40 flex-col justify-around items-center text-center gap-2">
                 <img src={item.image} alt={item.title} className="w-32"/>
-                <h2 className="text-md font-semibold">{item.title}</h2>
+                <h2 className="text-lg font-semibold ">{item.title}</h2>
             </div>
             <div className="flex flex-col justify-center items-center gap-2">
-                <h2>${item.price}</h2>
+                <h2 className="font-semibold">${item.price}</h2>
                 <div>
-                    <button>ADD TO CART</button>
-                    {/* <div>
-                        <button>-</button>
-                        <h2>0</h2>
-                        <button>+</button>
+                    <button className="border-[1px] border-[#000] px-2 py-[4px] rounded-full text-sm">ADD TO CART</button>
+                    {/* <div className="flex gap-6 border-[1px] border-[#000] px-4 py-[4px] rounded-full text-sm">
+                        <button className="text-md">-</button>
+                        <h2 className="text-md">0</h2>
+                        <button className="text-md">+</button>
                     </div> */}
                 </div>
             </div>
